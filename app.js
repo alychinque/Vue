6368183a2e76cd1d8117c74f42e341f6c7731d53
alychinque <alychinque@gmail.com>
2022-05-23 12:49:47 +0100

    outputting lists

--- app.js
+++ app.js
@@ -5,23 +5,16 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      title: 'The final empire',
-      author: 'Brandon Sanderson',
-      age: 45,
-      x: 0,
-      y:0
+      books: [
+        { title: 'name of the wind', author: 'patrick rothfuss' },
+        { title: 'the way of kings', author: 'brandon sanderson' },
+        { title: 'the final empire', author: 'brandon sanderson' },
+      ]
     }
   },
   methods: {
-    handleEvent(e, num){
-      console.log(num, e.type)
-    },
     toggleShowBooks(){
       this.showBooks = !this.showBooks
-    },
-    handleMouseMove(e){
-      this.x = e.offsetX
-      this.y = e.offsetY
     }
   }
 })
